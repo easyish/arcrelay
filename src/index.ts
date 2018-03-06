44678176a5878b64f37fb886e4354249cb8be5da
easyish <ishivonmeier@yahoo.com>
2018-03-05 16:50:51 -0800

    added todos to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,19 @@ import * as Web3 from 'web3';
 
 const TESTRPC_NETWORK_ID = 50;
 
+//TODO: encapsulate this into "create", "sign", and "fill" functions
+// (figure out if/how this can be done asynchronously)
+
+//TODO: figure out how to call these functions from javascript code inside index.html
+
+//TODO: Read through the OrderWatcher documentation and use it to implement order pruning
+
+//TODO Have create, sign, fill order functionality working through html page with amounts, token types, and addresses being input
+
+//TODO: Abstract the order creation code to work for any ERC20 token, or at least a much larger subset (current code just does ZRX <=> ETH)
+
+//TODO:Error handling for "Error: Invalid JSON RPC response: " --- this means testRPC is not running
+
 //
 //instantiate a zeroEx and a HttpProvider
 //
@@ -17,15 +30,31 @@ const configs = {
 const zeroEx = new ZeroEx(provider, configs);
 
 
-const setupAddresses = async () => {
-    // set contract and exchange addresses
-    const WETH_ADDRESS = zeroEx.etherToken.getContractAddressIfExists() as string; // The wrapped ETH token contract
-    const ZRX_ADDRESS = zeroEx.exchange.getZRXTokenAddress(); // The ZRX token contract
-    // The Exchange.sol address (0x exchange smart contract)
-    const EXCHANGE_ADDRESS = zeroEx.exchange.getContractAddress();
-};
+// set contract and exchange addresses
+const WETH_ADDRESS = zeroEx.etherToken.getContractAddressIfExists() as string; // The wrapped ETH token contract
+const ZRX_ADDRESS = zeroEx.exchange.getZRXTokenAddress(); // The ZRX token contract
+// The Exchange.sol address (0x exchange smart contract)
+const EXCHANGE_ADDRESS = zeroEx.exchange.getContractAddress();
 
-const setAllowances = async () => {
+
+
+//Ethereum Virtual Machine doesn't use decimals so we need to set DECIMALS
+//to convert our amounts with BigNumber()
+// Number of decimals to use (for ETH and ZRX)
+const DECIMALS = 18;
+
+
+
+const mainAsync = async () => {
+
+    //hard code in accounts from testRPC:  (replace with user input addresses later )
+    // Getting list of accounts
+    const accounts = await zeroEx.getAvailableAddressesAsync();
+    console.log('accounts: ', accounts);
+
+    // Set maker and takers to the first 2 things in the testRPC accounts
+    const [makerAddress, takerAddress] = accounts;
+
     //set allowances
     // Unlimited allowances to 0x proxy contract for maker and taker
     const setMakerAllowTxHash = await zeroEx.token.setUnlimitedProxyAllowanceAsync(ZRX_ADDRESS, makerAddress);
@@ -34,28 +63,6 @@ const setAllowances = async () => {
     const setTakerAllowTxHash = await zeroEx.token.setUnlimitedProxyAllowanceAsync(WETH_ADDRESS, takerAddress);
     await zeroEx.awaitTransactionMinedAsync(setTakerAllowTxHash);
     console.log('Taker allowance mined...');
-};
-
-//Ethereum Virtual Machine doesn't use decimals so we need to set DECIMALS
-//to convert our amounts with BigNumber()
-// Number of decimals to use (for ETH and ZRX)
-const DECIMALS = 18;
-
-
-//hard code in accounts from testRPC:  (replace with user input addresses later )
-// Getting list of accounts
-const accounts = await zeroEx.getAvailableAddressesAsync();
-console.log('accounts: ', accounts);
-
-// Set maker and takers to the first 2 things in the testRPC accounts
-const [makerAddress, takerAddress] = accounts;
-
-
-
-const mainAsync = async () => {
-
-    setupAddresses();
-    setAllowances();
 
     // Deposit WETH
     const ethAmount = new BigNumber(1);
