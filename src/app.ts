f12cb8fa4a2e4ed6bdd5590e9c1eaf3aac520a5f
easyish <ishivonmeier@yahoo.com>
2018-03-13 15:53:13 -0700

    stuff

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as cors from "cors";
 
 class App {
   public express
@@ -10,13 +11,30 @@ class App {
 
   private mountRoutes (): void {
     const router = express.Router()
+
+    const options:cors.CorsOptions = {
+      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+      credentials: true,
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      origin: 'http://localhost:3000',
+      preflightContinue: false
+    };
+
+    //use cors middleware
+    router.use(cors(options));
+
+    //add your routes
     router.get('/', (req, res) => {
       res.json({
         message: 'Hello World!'
       })
     })
     this.express.use('/', router)
-  }
+
+
+    //enable pre-flight
+    router.options("*", cors(options));
+    }
 }
 
 export default new App().express
